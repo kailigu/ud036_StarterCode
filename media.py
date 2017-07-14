5cee8a9f84c812181fe45ada7a83261c729f2907
Kaili Gu <kgux68@gmail.com>
2017-07-14 18:55:01 +1000

    Add files via upload

--- media.py
+++ media.py
@@ -1,6 +1,5 @@
 class Movie():
-    def __init__(self,title,image,trailer):
+    def __init__(self, title, image, trailer):
         self.title = title
         self.poster_image_url = image
         self.trailer_youtube_url = trailer
-
